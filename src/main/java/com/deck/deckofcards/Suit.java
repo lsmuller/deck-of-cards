89f5412ac962547e60abf9e6fb34ca5d4796322a
Leticia Muller <leticia.muller@sap.com>
2019-06-02 21:00:56 -0300

    created enum

---
+++ src/main/java/com/deck/deckofcards/Suit.java
@@ -0,0 +1,28 @@
+package com.deck.deckofcards;
+
+public enum Suit {
+	Club(0), Diamond(1), Heart(2), Spade(3);
+
+	private int value;
+
+	private Suit(int v) {
+		this.value = v;
+	}
+
+	public int getValue() { return value; }
+
+	public static Suit getSuitFromValue(int value) {
+		switch (value) {
+		case 0:
+			return Club;
+		case 1:
+			return Diamond;
+		case 2:
+			return Heart;
+		case 3:
+			return Spade;
+		default:
+			return null;
+		}
+	}
+}
